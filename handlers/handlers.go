cd3ccb587bc37f3d3d91a12c9127d9f045c98ece
weakinyi <akinyiwendy46@gmail.com>
2024-08-20 11:57:51 +0300

    feat: add location handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,20 +1,45 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 	"text/template"
 
 	groupie "groupie/data"
 )
 
+var Temp, err = template.ParseGlob("template/*.html")
+
+type Info struct {
+	Title string
+	Data  interface{}
+}
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	temp, err := template.ParseFiles("template/home.html")
 	if err != nil {
+		fmt.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	artist := groupie.FetchArtist()
+	var data Info
+	data.Title = "home"
+	data.Data = artist
 
-	temp.Execute(w, artist)
+	Temp.Execute(w, data)
+}
+
+func LocationHandler(w http.ResponseWriter, r *http.Request) {
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	location := groupie.FetchLocation()
+	var data Info
+	data.Title = "location"
+	data.Data = location
+
+	Temp.Execute(w, data)
 }
