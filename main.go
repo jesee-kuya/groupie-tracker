2f73ea21b714d04d05a044f4cc6b6d98409f0d95
josotieno <jokumu25@gmail.com>
2024-08-21 11:21:38 +0300

    Feat: Handle static files

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", handle.HomeHandler)
 	http.HandleFunc("/artist", handle.ArtistHandler)
 	server.ListenAndServe()
