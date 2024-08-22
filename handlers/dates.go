e476877b3603d4a68fe51d2257ecdabb56188a1b
jkuya <kuyajesee@proton.me>
2024-08-22 17:55:33 +0300

    Feat: Add handler for dates

---
+++ handlers/dates.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	groupie "groupie/data"
+)
+
+func DatesHandler(w http.ResponseWriter, r *http.Request) {
+	var data Info
+	if err != nil {
+		ErrorPage(w, r, http.StatusInternalServerError, "Internal server error")
+		return
+	}
+
+	strId := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(strId)
+	if err != nil {
+		ErrorPage(w, r, http.StatusNotFound, "Not found")
+		return
+	}
+
+	dates := groupie.FetchDates()
+	if id > len(dates.Index) {
+		ErrorPage(w, r, http.StatusNotFound, "Not found")
+		return
+	}
+	data.Title = "dates"
+	data.Data = dates.Index[id-1]
+
+	Temp.ExecuteTemplate(w, "base.html", data)
+}
