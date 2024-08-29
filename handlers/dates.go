12019f2004d15eb6cf9bdc5e9da67fdb8769bce8
JosephOkumu <jokumu25@gmail.com>
2024-08-29 23:32:30 +0300

    Add comments

--- handlers/dates.go
+++ handlers/dates.go
@@ -6,25 +6,32 @@ import (
 	groupie "groupie/data"
 )
 
+// DateData holds the artist details and their concert dates for rendering.
 type DateData struct {
-	Details groupie.Artist
-	Dt      groupie.Indexxx
+	Details groupie.Artist  
+	Dt      groupie.Indexxx 
 }
 
+// DatesHandler handles the HTTP request for displaying concert dates for an artist.
 func DatesHandler(w http.ResponseWriter, r *http.Request) {
 	var data Info
 	var details DateData
 
+	// Retrieve the artist ID from the query parameters.
 	id := GetId(w, r)
 	if id <= 0 {
-		return
+		return 
 	}
 
-	details.Details = Artiste[id-1]
-	details.Dt = Tarehe.Index[id-1]
+	// Assign the artist details and their concert dates to the DateData struct.
+	details.Details = Artiste[id-1]    
+	details.Dt = Tarehe.Index[id-1]   
+
+	// Set the title and data for the template.
 	data.Title = "dates"
 	data.Data = details
 
+	// Set HTTP status to OK and render the template with the data.
 	w.WriteHeader(http.StatusOK)
 	Temp.ExecuteTemplate(w, "base.html", data)
 }
--- handlers/location.go
+++ handlers/location.go
@@ -6,26 +6,34 @@ import (
 	groupie "groupie/data"
 )
 
+// LocData holds the data for an artist and their locations.
 type LocData struct {
-	Details groupie.Artist
-	Loc     groupie.Indexx
+	Details groupie.Artist  
+	Loc     groupie.Indexx  
 }
 
+// LocationtHandler handles requests to the location page for an artist.
 func LocationtHandler(w http.ResponseWriter, r *http.Request) {
 	var data Info
 	var details LocData
 
+	
 	id := GetId(w, r)
 	if id <= 0 {
-		return
+		return  
 	}
 
+	// Populate the LocData struct with artist details and locations.
 	details.Details = Artiste[id-1]
 	details.Loc = Emplacement.Index[id-1]
 
+	// Set the title for the location page.
 	data.Title = "location"
-	data.Data = details
 
+	// Assign the LocData struct to the page data.
+	data.Data = details
+	
 	w.WriteHeader(http.StatusOK)
+	
 	Temp.ExecuteTemplate(w, "base.html", data)
 }
