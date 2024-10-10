4aa9536dab6837c0489d3dffb7b405d492fa7a46
JosephOkumu <jokumu25@gmail.com>
2024-10-10 16:41:31 +0300

    refactor: modify code

--- handlers/suggestions.go
+++ handlers/suggestions.go
@@ -9,16 +9,16 @@ import(
 
 // SuggestionHandler handles suggestions for artists based on a query parameter.
 func SuggestionHandler(w http.ResponseWriter, r *http.Request) {
-	var results []groupie.Artist
-	word := r.URL.Query().Get("query")
+    var results []groupie.Artist
+    word := r.URL.Query().Get("query")
 
-	for _, artist := range Artiste {
-		if strings.Contains(strings.ToLower(artist.Name), strings.ToLower(word)) {
-			results = append(results, artist) // Add matching artist to results.
-		}
-	}
+    for _, artist := range Artiste {
+        if strings.Contains(strings.ToLower(artist.Name), strings.ToLower(word)) {
+            results = append(results, artist)
+        }
+    }
 
-	// Return the results as JSON.
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(results)
+    // Return the results as JSON.
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(results)
 }
