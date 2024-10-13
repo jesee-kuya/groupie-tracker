943af354e94146f3d7a0d731bee28c53a3cae35f
JosephOkumu <jokumu25@gmail.com>
2024-10-13 08:40:20 +0300

    chore: modify search to be more dynamic

--- handlers/search.go
+++ handlers/search.go
@@ -1,29 +1,75 @@
 package handlers
 
 import (
-	"net/http"
-	"strings"
-
-	groupie "groupie/data"
+    "net/http"
+    "strings"
+    "strconv"
+    groupie "groupie/data"
 )
 
-// SearchHandler handles search requests for artists based on a query parameter.
+type SearchResult struct {
+    Artist groupie.Artist
+    MatchType string
+    MatchedItem string
+}
+
 func SearchHandler(w http.ResponseWriter, r *http.Request) {
-	var data Info
-	var results []groupie.Artist
-	word := r.URL.Query().Get("query")
+    var data Info
+    var results []SearchResult
+    word := r.URL.Query().Get("query")
+    lowercaseWord := strings.ToLower(word)
 
-	for _, artist := range Artiste {
-		if strings.Contains(strings.ToLower(artist.Name), strings.ToLower(word)) {
-			results = append(results, artist) // Add matching artist to results.
-		}
-	}
+    for _, artist := range Artiste {
+        matches := matchArtist(artist, lowercaseWord)
+        results = append(results, matches...)
+    }
 
-	if results == nil {
-		ErrorPage(w, r, http.StatusNotFound, "Not found")
-		return
-	}
-	data.Title = "search"
-	data.Data = results
-	Temp.ExecuteTemplate(w, "base.html", data)
+    if len(results) == 0 {
+        ErrorPage(w, r, http.StatusNotFound, "Not found")
+        return
+    }
+
+    data.Title = "search"
+    data.Data = results
+    Temp.ExecuteTemplate(w, "base.html", data)
+}
+
+func matchArtist(artist groupie.Artist, query string) []SearchResult {
+    var matches []SearchResult
+
+    if strings.Contains(strings.ToLower(artist.Name), query) {
+        matches = append(matches, SearchResult{Artist: artist, MatchType: "Artist", MatchedItem: artist.Name})
+    }
+
+    for _, member := range artist.Members {
+        if strings.Contains(strings.ToLower(member), query) {
+            matches = append(matches, SearchResult{Artist: artist, MatchType: "Member", MatchedItem: member})
+        }
+    }
+
+    if matchesDate(artist.FirstAlbum, query) {
+        matches = append(matches, SearchResult{Artist: artist, MatchType: "First Album", MatchedItem: artist.FirstAlbum})
+    }
+
+    if matchesDate(strconv.Itoa(artist.CreationDate), query) {
+        matches = append(matches, SearchResult{Artist: artist, MatchType: "Creation Date", MatchedItem: strconv.Itoa(artist.CreationDate)})
+    }
+    
+    // Search through locations
+    for _, location := range Emplacement.Index {
+        if location.Id == artist.Id {
+            for _, loc := range location.Locations {
+                if strings.Contains(strings.ToLower(loc), query) {
+                    matches = append(matches, SearchResult{Artist: artist, MatchType: "Location", MatchedItem: loc})
+                }
+            }
+            break
+        }
+    }
+    
+    return matches
+}
+
+func matchesDate(date string, query string) bool {
+    return strings.Contains(strings.ToLower(date), query)
 }
