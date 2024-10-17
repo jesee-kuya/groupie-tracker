54b5ed0a78f7edc0aacc5385b935f27c5068963b
weakinyi <akinyiwendy46@gmail.com>
2024-10-17 12:06:43 +0300

    test: add test files

--- data/fetch_test.go
+++ data/fetch_test.go
@@ -81,37 +81,89 @@ func TestFetchArtist(t *testing.T) {
 	}
 }
 
-// func TestFetchLocation(t *testing.T) {
-// 	type Location struct {
-// 		Index []Indexx `json:"index"`
-// 	}
-	
-// 	tests := []struct {
-// 		name    string
-// 		want    Location
-// 		wantErr bool
-// 	}{
-// 		// TODO: Add test cases.
-// 		{"Mac Miller", Location{
-// 			Index: []Indexx{
-// 				{
-// 					Id: 7, 
-// 					Locations: []string{0: 	"california-usa", 1: "arizona-usa", 2: "texas-usa"}, 
-// 					Dates: "https://groupietrackers.herokuapp.com/api/dates/7"},
-// 			    },
-// 			}, 
-// 		false},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			got, err := FetchLocation()
-// 			if (err != nil) != tt.wantErr {
-// 				t.Errorf("FetchLocation() error = %v, wantErr %v", err, tt.wantErr)
-// 				return
-// 			}
-// 			if !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("FetchLocation() = %v,\n want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
+func TestFetchLocation(t *testing.T) {
+	type Location struct {
+		Index []Indexx `json:"index"`
+	}
+
+	tests := []struct {
+		name    string
+		want    Location
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			"Mac Miller",
+			Location{
+				Index: []Indexx{
+					{
+						Id:        7,
+						Locations: []string{"california-usa", "arizona-usa", "texas-usa"},
+						Dates:     "https://groupietrackers.herokuapp.com/api/dates/7",
+					},
+				},
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FetchLocation()
+			got.Index = got.Index[6:7]
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FetchLocation() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got.Index, tt.want.Index) {
+				t.Errorf("FetchLocation() = %v,\n want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFetchDates(t *testing.T) {
+	type Dates struct {
+		Index []Indexxx `json:"index"`
+	}
+
+	tests := []struct {
+		name    string
+		want    Dates
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "SOJA",
+			want: Dates{Index: []Indexxx{
+				{
+					Id: 2,
+					Dates: []string{
+						"*05-12-2019",
+						"06-12-2019",
+						"07-12-2019",
+						"08-12-2019",
+						"09-12-2019",
+						"*16-11-2019",
+						"*15-11-2019",
+					},
+				},
+			}},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FetchDates()
+			got.Index = got.Index[1:2]
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FetchDates() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got.Index, tt.want.Index) {
+				t.Errorf("FetchDates() = %v,\n want %v", got, tt.want)
+			}
+		})
+	}
+}
