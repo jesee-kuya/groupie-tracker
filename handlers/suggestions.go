6d1e09f319160933d5b8c75f8090a659049becac
weakinyi <akinyiwendy46@gmail.com>
2024-10-21 09:19:26 +0300

    style: format the files

--- handlers/suggestions.go
+++ handlers/suggestions.go
@@ -1,32 +1,35 @@
 package handlers
+
 import (
-    "encoding/json"
-    "net/http"
-    "strings"
+	"encoding/json"
+	"net/http"
+	"strings"
 )
+
 type SuggestionResult struct {
-    ArtistId   int    `json:"artistId"`
-    Name       string `json:"name"`
-    MatchType  string `json:"matchType"`
-    MatchedItem string `json:"matchedItem"`
-    ArtistName string `json:"artistName"` 
+	ArtistId    int    `json:"artistId"`
+	Name        string `json:"name"`
+	MatchType   string `json:"matchType"`
+	MatchedItem string `json:"matchedItem"`
+	ArtistName  string `json:"artistName"`
 }
+
 func SuggestionHandler(w http.ResponseWriter, r *http.Request) {
-    var results []SuggestionResult
-    word := r.URL.Query().Get("query")
-    lowercaseWord := strings.ToLower(word)
-    for _, artist := range Artiste {
-        matches := matchArtist(artist, lowercaseWord)
-        for _, match := range matches {
-            results = append(results, SuggestionResult{
-                ArtistId: artist.Id,
-                Name: artist.Name,
-                MatchType: match.MatchType,
-                MatchedItem: match.MatchedItem,
-                ArtistName: artist.Name,
-            })            
-        }
-    }
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(results)
+	var results []SuggestionResult
+	word := r.URL.Query().Get("query")
+	lowercaseWord := strings.ToLower(word)
+	for _, artist := range Artiste {
+		matches := matchArtist(artist, lowercaseWord)
+		for _, match := range matches {
+			results = append(results, SuggestionResult{
+				ArtistId:    artist.Id,
+				Name:        artist.Name,
+				MatchType:   match.MatchType,
+				MatchedItem: match.MatchedItem,
+				ArtistName:  artist.Name,
+			})
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(results)
 }
