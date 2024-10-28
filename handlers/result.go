fe52b777164ef5002e45ec19e2e8128b61044db8
jkuya <kuyajesee@proton.me>
2024-10-28 16:10:29 +0300

    Feat: add a AlbumYear function to handle album years range

--- handlers/result.go
+++ handlers/result.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	groupie "groupie/data"
 	"net/http"
+	"strconv"
 )
 
 func Results(w http.ResponseWriter, r *http.Request) {
@@ -11,18 +12,36 @@ func Results(w http.ResponseWriter, r *http.Request) {
 		ErrorPage(w, r, http.StatusInternalServerError, "Internal Server Error")
 		return
 	}
-	creationFrm := r.Form.Get("creationDateFrom")
-	creationTo := r.Form.Get("creationDateTo")
-	albumFromYear := r.Form.Get("albumDateFrom")
-	albumtoYear := r.Form.Get("albumDateTo")
-	minMembers := r.Form.Get("membersMin")
-	maxMembers := r.Form.Get("membersMax")
-	location := r.Form.Get("location")
+	creationFrm, err := strconv.Atoi(r.Form.Get("creationDateFrom"))
+	creationTo, err := strconv.Atoi(r.Form.Get("creationDateTo"))
+	albumFromYear, err:= strconv.Atoi(r.Form.Get("albumDateFrom"))
+	albumtoYear, err := strconv.Atoi(r.Form.Get("albumDateTo"))
+	minMembers, err := strconv.Atoi(r.Form.Get("membersMin"))
+	maxMembers, err := strconv.Atoi(r.Form.Get("membersMax"))
+	location, err := strconv.Atoi(r.Form.Get("location"))
+
+	if err != nil {
+		ErrorPage(w, r, http.StatusBadRequest, "Bad request")
+	}
+	
+	result = CreationDate(creationFrm, creationTo)
 }
 
 func CreationDate(creationFrm, creationTo int) (result []groupie.Artist) {
 	for _ , artist := range Artiste {
 		if artist.CreationDate >= creationFrm && artist.CreationDate <= creationTo {
+			result = append(result, artist)
+		}
+	}
+	return
+}
+
+func AlbumYear(res []groupie.Artist, AlbumFrm, AlbumTo int) (result []groupie.Artist) {
+	if res == nil {
+		res = Artiste
+	}
+	for _ , artist := range res {
+		if artist.CreationDate >= AlbumFrm && artist.CreationDate <=AlbumTo {
 			result = append(result, artist)
 		}
 	}
