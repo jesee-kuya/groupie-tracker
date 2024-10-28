2bbe13146716bf95730009379f8c39f936f63751
jkuya <kuyajesee@proton.me>
2024-10-28 12:53:40 +0300

    Feat: code the filter handler

--- handlers/filter.go
+++ handlers/filter.go
@@ -3,5 +3,8 @@ package handlers
 import "net/http"
 
 func Filter(w http.ResponseWriter, r *http.Request) {
-	
+	var data Info
+	data.Title = "filter"
+	w.WriteHeader(http.StatusOK)
+	Temp.ExecuteTemplate(w, "base.html", data)
 }
