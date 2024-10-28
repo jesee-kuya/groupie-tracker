28f035527c8b325cd6cbd5f07f6b1b9d8d7d8a1a
jkuya <kuyajesee@proton.me>
2024-10-28 12:46:04 +0300

    Feat: handle end route for filter

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -89,6 +89,13 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			ErrorPage(w, r, http.StatusMethodNotAllowed, "Method not allowed")
 			return
 		}
+	} else if r.URL.Path == "/filter" {
+		if r.Method != "GET" {
+			ErrorPage(w, r, http.StatusMethodNotAllowed, "Method not allowed")
+			return
+		}
+		Filter(w, r)
+		return
 	} else {
 		ErrorPage(w, r, http.StatusNotFound, "Not found")
 		return
