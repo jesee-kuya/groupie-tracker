9f65008c59bada4c4965554207a36c65034b271c
weakinyi <akinyiwendy46@gmail.com>
2024-11-04 09:56:39 +0300

    Feat: handle errors

--- handlers/result.go
+++ handlers/result.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
@@ -15,10 +16,10 @@ func Results(w http.ResponseWriter, r *http.Request) {
 		ErrorPage(w, r, http.StatusInternalServerError, "Internal Server Error")
 		return
 	}
-	creationFrm, _ := strconv.Atoi(r.Form.Get("creationDateFrom"))
-	creationTo, _ := strconv.Atoi(r.Form.Get("creationDateTo"))
-	albumFrm, _ := strconv.Atoi(r.Form.Get("albumDateFrom"))
-	albumTo, _ := strconv.Atoi(r.Form.Get("albumDateTo"))
+	creationFrm, err1 := strconv.Atoi(r.Form.Get("creationDateFrom"))
+	creationTo, err2 := strconv.Atoi(r.Form.Get("creationDateTo"))
+	albumFrm, err3 := strconv.Atoi(r.Form.Get("albumDateFrom"))
+	albumTo, err4 := strconv.Atoi(r.Form.Get("albumDateTo"))
 	location := r.Form.Get("location")
 	memb1 := r.Form.Get("members1")
 	memb2 := r.Form.Get("members2")
@@ -33,10 +34,19 @@ func Results(w http.ResponseWriter, r *http.Request) {
 
 	members := CheckMembers(memb1, memb2, memb3, memb4, memb5, memb6, memb7, memb8, memb9, memb10Plus)
 
+	if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
+		ErrorPage(w, r, http.StatusBadRequest, "Bad request")
+		return
+	}
+
 	result = CreationDate(creationFrm, creationTo)
 	result = AlbumYear(result, albumFrm, albumTo)
 	result = Members(result, members)
+	fmt.Println("Members")
+	fmt.Println(result)
+	fmt.Println("Out of members")
 	result = SearchLocation(result, location)
+	fmt.Println(result)
 
 	data.Title = "result"
 	data.Data = result
@@ -60,7 +70,7 @@ func CreationDate(creationFrm, creationTo int) (result []groupie.Artist) {
 
 func AlbumYear(res []groupie.Artist, AlbumFrm, AlbumTo int) (result []groupie.Artist) {
 	if res == nil {
-		return
+		return res
 	}
 	for _, artist := range res {
 		num, _ := strconv.Atoi(artist.FirstAlbum[len(artist.FirstAlbum)-4:])
@@ -72,8 +82,8 @@ func AlbumYear(res []groupie.Artist, AlbumFrm, AlbumTo int) (result []groupie.Ar
 }
 
 func Members(res []groupie.Artist, members []int) (result []groupie.Artist) {
-	if res == nil {
-		return
+	if res == nil || members == nil {
+		return res
 	}
 	for _, artist := range res {
 		for _, num := range members {
@@ -88,7 +98,7 @@ func Members(res []groupie.Artist, members []int) (result []groupie.Artist) {
 
 func SearchLocation(res []groupie.Artist, location string) (result []groupie.Artist) {
 	if res == nil {
-		return
+		return res
 	}
 	if location == "" {
 		return res
@@ -97,12 +107,11 @@ func SearchLocation(res []groupie.Artist, location string) (result []groupie.Art
 		for _, locale := range Emplacement.Index {
 			if locale.Id == artist.Id {
 				for _, loc := range locale.Locations {
-					if strings.Contains(strings.ToLower(loc), location) {
+					if strings.Contains(strings.ToLower(loc), strings.ToLower(location)) {
 						result = append(result, artist)
 						break
 					}
 				}
-				break
 			}
 		}
 	}
